29d9e8404be603bea3552e3432594b9e690428e8
tanakakeitaro <1968kocm1akouk@gmail.com>
2019-09-26 19:52:00 +0900

    ファイルの変更

--- python_file/scraping_show.py
+++ python_file/scraping_show.py
@@ -5,5 +5,5 @@ import matplotlib.pyplot as plt
 import io,sys
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-df = pd.read_csv("datasets/test3_2.csv", encoding="shift-jis")
+df = pd.read_csv("../datasets/issa_data.csv", encoding="shift-jis")
 print(df.iloc[12,2])
